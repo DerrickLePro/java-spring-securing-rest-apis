daf596a50b1ccada07b44df9fa52962a7b7b8a9a
KAFFO <kaffoderrick@gmail.com>
2020-10-01 11:26:08 +0100

    feat: module 2

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionRepository.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionRepository.java
@@ -3,6 +3,7 @@ package io.jzheaux.springsecurity.resolutions;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,11 +11,11 @@ import java.util.UUID;
 
 @Repository
 public interface ResolutionRepository extends CrudRepository<Resolution, UUID> {
-	@Modifying
-	@Query("UPDATE Resolution SET text = :text WHERE id = :id")
-	void revise(UUID id, String text);
+    @Modifying
+    @Query("UPDATE Resolution SET text = :text WHERE id = :id AND owner = ?#{authentication.name}")
+    void revise(@Param("id") UUID id, @Param("text") String text);
 
-	@Modifying
-	@Query("UPDATE Resolution SET completed = 1 WHERE id = :id")
-	void complete(UUID id);
+    @Modifying
+    @Query("UPDATE Resolution SET completed = 1 WHERE id = :id AND owner = ?#{authentication.name}")
+    void complete(@Param("id") UUID id);
 }
